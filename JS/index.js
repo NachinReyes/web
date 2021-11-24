77f27b9448c2e8912450b87de8792f2f3b432de1
NachinReyes <83214184+NachinReyes@users.noreply.github.com>
2021-11-24 13:07:04 +0100

    Add files via upload

--- JS/index.js
+++ JS/index.js
@@ -1,21 +1,26 @@
 window.sr=ScrollReveal();
 
 sr.reveal(".container-slider",{
-    duration: 3000,
+    duration: 2000,
     origin:"bottom",
     distance: "1px"
 });
 
 sr.reveal("#articulos",{
-    duration: 3000,
-    origin:"left",
-    distance: "1px"
+    duration: 2000,
+    origin:"bottom",
+    distance: "200px"
 });
 
+sr.reveal("footer",{
+    duration: 2000,
+    origin:"bottom",
+    distance: "200px"
+});
 /******************PAGINA DE CONTACTOS************* */
-
 sr.reveal(".contacto__form",{
-    duration: 3000,
+    duration: 2000,
     origin:"left",
     distance: "1px"
 });
+
