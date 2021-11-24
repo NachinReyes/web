9bb5668447d528e614392c278a72b45ce8e9eab3
NachinReyes <83214184+NachinReyes@users.noreply.github.com>
2021-11-24 13:37:23 +0100

    Add files via upload

--- JS/menu.js
+++ JS/menu.js
@@ -1,19 +1,37 @@
 let menu =document.getElementById("barra");
 let cerrar = document.getElementById("cerrar");
 
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("down", window.scrollY>200);
+})
+
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("up", window.scrollY<200);
+})
 
 menu.onclick = function(){
     menu.style.display="none"
     cerrar.style.display="block";
+    
 }
 
 cerrar.onclick = function(){
     cerrar.style.display="none";
     menu.style.display="block" 
+    
 }
+
+var tamanoPantalla =Screen.Width=1024;
+if(tamanoPantalla >1024){
+    console.log("hola");
+    menu.style.display="none";
+    cerrar.style.display="none";
+}
+
 
 document.getElementById("logo").onclick = function (){
     window.location.href="../index.html";
 } 
-
 
