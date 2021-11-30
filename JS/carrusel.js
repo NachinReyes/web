9e7e94968ba4e89009e6ecdc62f2060058ebb8d5
NachinReyes <83214184+NachinReyes@users.noreply.github.com>
2021-11-30 05:16:53 +0100

    Update carrusel.js

--- JS/carrusel.js
+++ JS/carrusel.js
@@ -14,7 +14,7 @@ function next(){
         /*slider.style.transition= "none"*/
         slider.insertAdjacentElement('beforeend', sliderSectionFirst);
         slider.style.marginLeft = "-100%";
-    },1)
+    },0)
 
 }
 
@@ -31,7 +31,7 @@ function prev (){
         /*slider.style.transition= "none"*/
         slider.insertAdjacentElement('afterbegin', sliderSectionLast);
         slider.style.marginLeft = "-100%";
-    },1)
+    },0)
 
 }
 
