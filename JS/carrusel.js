64a240d71c0ec76fb983c6d5ae56803b792eb6bd
NachinReyes <83214184+NachinReyes@users.noreply.github.com>
2021-11-30 04:27:46 +0100

    Update carrusel.js

--- JS/carrusel.js
+++ JS/carrusel.js
@@ -9,12 +9,12 @@ slider.insertAdjacentElement('afterbegin', sliderSectionLast);
 function next(){
     let sliderSectionFirst = document.querySelectorAll(".slider-section")[0];
     slider.style.marginLeft = "-200%";
-    slider.style.transition = "all 2s";
+    /*slider.style.transition = "all 2s";*/
     setTimeout(function(){
         slider.style.transition= "none"
         slider.insertAdjacentElement('beforeend', sliderSectionFirst);
         slider.style.marginLeft = "-100%";
-    },2000)
+    },2)
 
 }
 
@@ -26,12 +26,12 @@ function prev (){
     let sliderSection = document.querySelectorAll(".slider-section");
     let sliderSectionLast = sliderSection[sliderSection.length -1];
     slider.style.marginLeft = "0";
-    slider.style.transition = "all 2s";
+    /*slider.style.transition = "all 2s";*/
     setTimeout(function(){
         slider.style.transition= "none"
         slider.insertAdjacentElement('afterbegin', sliderSectionLast);
         slider.style.marginLeft = "-100%";
-    },2000)
+    },2)
 
 }
 
