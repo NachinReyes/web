af0e0c2c2818f08768bd8a305aad060b1a42d6ae
NachinReyes <83214184+NachinReyes@users.noreply.github.com>
2021-12-04 08:20:38 +0100

    Update menu.js

--- JS/menu.js
+++ JS/menu.js
@@ -26,29 +26,3 @@ document.getElementById("logo").onclick = function (){
 btnReserva.onclick=function(){
     window.location.href="HTML/reservas.html";
 }
-/*var div = document.getElementById('capa');
-var but = document.getElementById('boton');*/
-
-//la funcion que oculta y muestra
-function showHide(e){
-e.preventDefault();
-e.stopPropagation();
-if(menu.style.display == "none"){
-cerrar.style.display = "block";
-} else if(cerrar.style.display == "block"){
-menu.style.display = "none";
-}
-}
-//al hacer click en el boton
-menu.addEventListener("click", showHide, false);
-
-//funcion para cualquier clic en el documento
-document.addEventListener("click", function(e){
-console.log('clic');
-//obtiendo informacion del DOM para  
-var clic = e.target;
-console.log(clic);
-if(cerrar.style.display == "block" && clic != cerrar){
- cerrar.style.display = "none";
-}
-}, false);
