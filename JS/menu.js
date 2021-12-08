6e1ca3ec9f42bdcc1bc471a7729b8cbd9979231c
NachinReyes <83214184+NachinReyes@users.noreply.github.com>
2021-12-08 14:45:36 +0100

    Update menu.js

--- JS/menu.js
+++ JS/menu.js
@@ -1,4 +1,4 @@
-let menu =document.getElementById("barra");
+Lolet menu =document.getElementById("barra");
 let cerrar = document.getElementById("cerrar");
 
 window.addEventListener("scroll", function(){
@@ -7,14 +7,14 @@ window.addEventListener("scroll", function(){
 })
 
 menu.onclick = function(){
-    menu.style.transform="rotate(45deg);
+    
     menu.style.display="none"
     cerrar.style.display="block";
     
 }
 
 cerrar.onclick = function(){
-    cerrar.style.transform="rotate(45deg)";
+
     cerrar.style.display="none";
     menu.style.display="block"; 
 }
