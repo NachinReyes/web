d6788d81bad9753c8cae6735aea84ee0f0898bf6
NachinReyes <83214184+NachinReyes@users.noreply.github.com>
2021-12-19 00:35:30 +0100

    Update mailContacto.php

--- PHP/mailContacto.php
+++ PHP/mailContacto.php
@@ -1,8 +1,8 @@
  <?php
     $destino = 'info@tourlibre.com';
-    $nombre = $_Post["nombre"];
-    $remitente = $_Post["remitente"];
-    $mensaje = $_Post["mensaje"];
+    $nombre = $_POST["nombre"];
+    $remitente = $_POST["remitente"];
+    $mensaje = $_POST["mensaje"];
     $contenido = "Nombre:" .$nombre . "\nRemitente:" .$remitente . "\nMensaje:" .$mensaje;
     mail($destino, "Contacto", $contenido);
     header("Location:..HTML/contacto.html");
