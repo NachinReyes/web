4114a1695da9a0009816081f0845886becf875a1
NachinReyes <83214184+NachinReyes@users.noreply.github.com>
2021-12-19 00:27:32 +0100

    Update mailContacto.php

--- PHP/mailContacto.php
+++ PHP/mailContacto.php
@@ -5,6 +5,6 @@
     $mensaje = $_Post["mensaje"];
     $contenido = "Nombre:" .$nombre . "\nRemitente:" .$remitente . "\nMensaje:" .$mensaje;
     mail($destino, "Contacto", $contenido);
-    header("<html> <h2>tTu mensaje a sido enviado, pronto nos pondremos en contacto contigo.</h2></html>");
+    header("Location:..HTML/contacto.html");
 
  ?>
